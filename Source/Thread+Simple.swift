863a35a3142c19735c2b4eda57ff1c944e1d4e30
kojirof <kojirof@gumob.com>
2018-12-07 16:26:09 +0900

    Pad string

--- Source/Thread+Simple.swift
+++ Source/Thread+Simple.swift
@@ -9,31 +9,40 @@
 import Foundation
 
 public extension Thread {
-
     class var simpleCallStackSymbols: [String] {
         guard let bundleName: String = CallStackParser.bundleName else { return [] }
-        return Thread.callStackSymbols
+        let symbols: [String] = Thread.callStackSymbols
                 .dropFirst()
                 .map {
-                    guard let module: String = $0.replacingOccurrences(of: "\\s+", with: " ", options: .regularExpression, range: nil).components(separatedBy: " ")[safe: 1],
+                    guard let module: String = $0.replacingOccurrences(of: "\\s+",
+                                                                       with: " ",
+                                                                       options: .regularExpression,
+                                                                       range: nil)
+                                                 .components(separatedBy: " ")[safe: 1],
                           module.hasPrefix(bundleName),
-                          let symbol: (String, String) = CallStackParser.classAndMethodForStackSymbol($0) else { return "" }
+                          let symbol: (String, String) = CallStackParser.classAndMethodForStackSymbol($0) else {
+                        return ""
+                    }
                     let classStr: String = symbol.0.padding(toLength: 36, withPad: " ", startingAt: 0)
                     return "\(classStr) \(symbol.1)"
                 }
                 .filter {
                     !$0.isEmpty
                 }
+        let count: Int = symbols.count
+        let digit: Int = String(count).count
+        return symbols
+                .reversed()
+                .enumerated()
+                .map {
+                    let index: String = String($0.0 + 1).leftPadding(toLength: digit, withPad: "0")
+                    let head: String = "[CallStack:\(index)/\(count)]"
+                    return "\(head) \($0.1)"
+                }
+                .reversed()
     }
 
     class var simpleCallStackString: String {
-        return simpleCallStackSymbols.reversed()
-                                     .enumerated()
-                                     .map {
-                                         let index: String = "[CallStack:\($0.0)]".padding(toLength: 16, withPad: " ", startingAt: 0)
-                                         return "\(index) \($0.1)"
-                                     }
-                                     .reversed()
-                                     .joined(separator: "\n")
+        return simpleCallStackSymbols.joined(separator: "\n")
     }
 }
